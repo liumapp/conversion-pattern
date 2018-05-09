bbfd89f8cf6a8bbbb531c376dcf59cbb19b526de
liumapp <liumapp.com@gmail.com>
2018-05-09 11:20:04 +0800

    add empty pattern files

---
+++ src/main/java/com/liumapp/pattern/conversion/AllPagePattern.java
@@ -0,0 +1,11 @@
+package com.liumapp.pattern.conversion;
+
+/**
+ * @author liumapp
+ * @file AllPagePattern.java
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 5/9/18
+ */
+public class AllPagePattern {
+}
---
+++ src/main/java/com/liumapp/pattern/conversion/DocResultPattern.java
@@ -0,0 +1,11 @@
+package com.liumapp.pattern.conversion;
+
+/**
+ * @author liumapp
+ * @file DocResultPattern.java
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 5/9/18
+ */
+public class DocResultPattern {
+}
