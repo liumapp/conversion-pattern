af5f567bad6453f7c0a285a1810c5d7fefe4eabc
liumapp <liumapp.com@gmail.com>
2018-05-09 18:18:04 +0800

    update multy pattern

--- src/main/java/com/liumapp/pattern/conversion/FirstPagePattern.java
+++ src/main/java/com/liumapp/pattern/conversion/FirstPagePattern.java
@@ -8,4 +8,72 @@ package com.liumapp.pattern.conversion;
  * @date 5/9/18
  */
 public class FirstPagePattern {
+
+    /**
+     * 1. oss
+     * 2. sys
+     */
+    private String type;
+
+    private String ossPath;
+
+    private String sysPath;
+
+    private String msg;
+
+    private boolean result;
+
+    public FirstPagePattern() {
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getOssPath() {
+        return ossPath;
+    }
+
+    public void setOssPath(String ossPath) {
+        this.ossPath = ossPath;
+    }
+
+    public String getSysPath() {
+        return sysPath;
+    }
+
+    public void setSysPath(String sysPath) {
+        this.sysPath = sysPath;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public boolean isResult() {
+        return result;
+    }
+
+    public void setResult(boolean result) {
+        this.result = result;
+    }
+
+    @Override
+    public String toString() {
+        return "FirstPagePattern{" +
+                "type='" + type + '\'' +
+                ", ossPath='" + ossPath + '\'' +
+                ", sysPath='" + sysPath + '\'' +
+                ", msg='" + msg + '\'' +
+                ", result='" + result + '\'' +
+                '}';
+    }
 }
--- src/main/java/com/liumapp/pattern/conversion/PagePattern.java
+++ src/main/java/com/liumapp/pattern/conversion/PagePattern.java
@@ -8,4 +8,61 @@ package com.liumapp.pattern.conversion;
  * @date 5/9/18
  */
 public class PagePattern {
+
+    /**
+     * 1. oss
+     * 2. sys
+     */
+    private String type;
+
+    private String sysPdfPath;
+
+    private String ossPdfPath;
+
+    private String imgPath;
+
+    public PagePattern() {
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getSysPdfPath() {
+        return sysPdfPath;
+    }
+
+    public void setSysPdfPath(String sysPdfPath) {
+        this.sysPdfPath = sysPdfPath;
+    }
+
+    public String getOssPdfPath() {
+        return ossPdfPath;
+    }
+
+    public void setOssPdfPath(String ossPdfPath) {
+        this.ossPdfPath = ossPdfPath;
+    }
+
+    public String getImgPath() {
+        return imgPath;
+    }
+
+    public void setImgPath(String imgPath) {
+        this.imgPath = imgPath;
+    }
+
+    @Override
+    public String toString() {
+        return "PagePattern{" +
+                "type='" + type + '\'' +
+                ", sysPdfPath='" + sysPdfPath + '\'' +
+                ", ossPdfPath='" + ossPdfPath + '\'' +
+                ", imgPath='" + imgPath + '\'' +
+                '}';
+    }
 }
