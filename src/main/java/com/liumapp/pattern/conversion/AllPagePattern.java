723d62e119eeafb9f6332d8081711db3911aff0d
liumapp <liumapp.com@gmail.com>
2018-05-09 18:50:18 +0800

    tmp

--- src/main/java/com/liumapp/pattern/conversion/AllPagePattern.java
+++ src/main/java/com/liumapp/pattern/conversion/AllPagePattern.java
@@ -8,4 +8,73 @@ package com.liumapp.pattern.conversion;
  * @date 5/9/18
  */
 public class AllPagePattern {
+
+    /**
+     * 1. oss
+     * 2. sys
+     */
+    private String type;
+
+    private String ossPath;
+
+    private String sysPath;
+
+    private String msg;
+
+    private boolean result;
+
+    public AllPagePattern() {
+
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getOssPath() {
+        return ossPath;
+    }
+
+    public void setOssPath(String ossPath) {
+        this.ossPath = ossPath;
+    }
+
+    public String getSysPath() {
+        return sysPath;
+    }
+
+    public void setSysPath(String sysPath) {
+        this.sysPath = sysPath;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public boolean isResult() {
+        return result;
+    }
+
+    public void setResult(boolean result) {
+        this.result = result;
+    }
+
+    @Override
+    public String toString() {
+        return "AllPagePattern{" +
+                "type='" + type + '\'' +
+                ", ossPath='" + ossPath + '\'' +
+                ", sysPath='" + sysPath + '\'' +
+                ", msg='" + msg + '\'' +
+                ", result=" + result +
+                '}';
+    }
 }
