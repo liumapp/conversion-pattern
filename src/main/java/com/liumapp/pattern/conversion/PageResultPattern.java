b7d64855ca2814cc78fa136e0f29c8cbbb2df1e6
liumapp <liumapp.com@gmail.com>
2018-05-09 11:34:33 +0800

    add fastjson dependency

---
+++ src/main/java/com/liumapp/pattern/conversion/PageResultPattern.java
@@ -0,0 +1,14 @@
+package com.liumapp.pattern.conversion;
+
+/**
+ * @author liumapp
+ * @file PageResultPattern.java
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 5/9/18
+ */
+public class PageResultPattern {
+
+
+
+}
